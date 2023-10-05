7544022c5d51f6aadd6fd8bbeb33694ad51d66be
Shreyas <shreyas@pop-os.localdomain>
2023-10-06 00:16:18 +0530

    Removed migrations folder

--- apps/server/src/prisma/migrations/20231003132124_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[questionId,userId]` on the table `Upvote` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Upvote_questionId_userId_key" ON "Upvote"("questionId", "userId");
